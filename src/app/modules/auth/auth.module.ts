e552c22559fa80173bdc0f3d76c91762d37ee984
NataliRodriguez <brendulis.2005@gmail.com>
2023-09-13 09:08:42 -0300

    mergeo solucinando problemas de actualizacion

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,7 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
+// MATERIAL
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+// FORMULARIO
+import { FormsModule } from '@angular/forms';
+
+// SERVICIOS
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
