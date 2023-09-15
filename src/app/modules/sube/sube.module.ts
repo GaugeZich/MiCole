8036409fe5d65ada158e39d98447c33c8e221cd6
GaugeZich <guilleop.vale@gmail.com>
2023-09-15 12:00:00 -0300

    Subo avance de vista sube

--- src/app/modules/sube/sube.module.ts
+++ src/app/modules/sube/sube.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SubeComponent } from './pages/sube/sube.component';
 
+import { SubeRoutingModule } from './sube-routing.module';
 
+// Importaciones de angular material
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SubeComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SubeRoutingModule,
+    MatGridListModule
+  ],
+  exports: [
+    SubeComponent
   ]
 })
 export class SubeModule { }
