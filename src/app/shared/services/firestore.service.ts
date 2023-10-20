d7ab6a7eb8639c2384d7a6e1a853e8576fa2a20f
luci0310 <lucianamartinez898@gmail.com>
2023-10-20 13:35:56 -0300

    subo la funcion para agregar usuarios

--- src/app/shared/services/firestore.service.ts
+++ src/app/shared/services/firestore.service.ts
@@ -6,9 +6,24 @@ import {Usuario} from 'src/app/models/usuario';
     providedIn: 'root'
 })
 export class FirestoreService{
+    // referenciamos colección en la base de datos
     private usuariosCollection: AngularFirestoreCollection<Usuario>
+
     constructor(private database: AngularFirestore){
         this.usuariosCollection=this.database.collection<Usuario>('usuarios')
     }
-    agregarUsuario(){}
+
+    
+    agregarUsuario(usuario: Usuario, id: string){
+    return new Promise(async(resolve, reject) => {
+      //capturamos los datos 
+      try{
+        usuario.uid = id;
+        const resultado = await this.usuariosCollection.doc(id).set(usuario);
+        resolve(resultado)
+      } catch (error){
+        reject(error)
+      }
+    })
+  }
 }
