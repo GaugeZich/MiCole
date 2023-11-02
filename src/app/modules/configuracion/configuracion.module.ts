7f97813f37027e940289edd76293286472960021
NataliRodriguez <brendulis.2005@gmail.com>
2023-11-02 10:53:39 -0300

    subo el modulo y el routing de configuracion

---
+++ src/app/modules/configuracion/configuracion.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ConfiguracionRoutingModule } from './configuracion-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ConfiguracionRoutingModule
+  ]
+})
+export class ConfiguracionModule { }
