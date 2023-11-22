fd06db68c3e4262b81745126348dab5bf7bffc42
GaugeZich <guilleop.vale@gmail.com>
2023-11-22 08:43:25 -0300

    Subo avance de codigo verificar admin

--- src/app/modules/inicio/pages/inicio/inicio.component.ts
+++ src/app/modules/inicio/pages/inicio/inicio.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from 'src/app/modules/admin/services/crud.service';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit{
   opened = false;
+  admin = false;
+
+  constructor(
+    private userService: AuthService,
+    private crud: CrudService
+  ){}
+
+  ngOnInit(): void{
+    this.userService.auth.currentUser.then((userLogeado) => {
+      this.crud.obtenerUsuario().subscribe(usuarios => {
+        if(userLogeado){
+          for (let i = 0; i < usuarios.length; i++) {
+            if(usuarios[i].uid == userLogeado.uid){
+              if(usuarios[i].rol == 'admin'){
+                this.admin = true;
+                break;
+              }
+            }
+          }
+        }
+      })
+    })
+  }
+
+
 }
