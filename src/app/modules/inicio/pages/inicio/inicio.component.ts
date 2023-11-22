05a865a85b295c778d75df3c0022f689aec10df4
GaugeZich <guilleop.vale@gmail.com>
2023-11-22 11:49:43 -0300

    El boton cuenta ahora identifica si el usuario tiene una sesion iniciada o no a traves de una funcion

--- src/app/modules/inicio/pages/inicio/inicio.component.ts
+++ src/app/modules/inicio/pages/inicio/inicio.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario';
 import { CrudService } from 'src/app/modules/admin/services/crud.service';
 import { AuthService } from 'src/app/modules/auth/services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-inicio',
@@ -12,20 +15,30 @@ export class InicioComponent implements OnInit{
   admin = false;
 
   constructor(
-    private userService: AuthService,
-    private crud: CrudService
+    private authService: AuthService,
+    private crud: CrudService,
+    private router: Router,
+    private auth: AngularFireAuth
   ){}
 
   ngOnInit(): void{
-    this.userService.auth.currentUser.then((userLogeado) => {
+    this.authService.auth.currentUser.then((userLogeado) => {
       this.crud.obtenerUsuario().subscribe(usuarios => {
         if (userLogeado) {
           const isAdmin = usuarios.find(usuario => usuario.uid === userLogeado.uid && usuario.rol === 'admin');
           this.admin = isAdmin !== undefined;
         }
       })
-    })
+    })    
   }
 
-
+  entrarCuenta(){
+    this.auth.authState.subscribe(usuarios => {
+      if(usuarios) {
+        this.router.navigate(['/cuenta'])
+      }else{
+        this.router.navigate(['/login'])
+      }
+    })
+  }
 }
