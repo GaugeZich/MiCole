af22dbb28a3402c60ed5d3cbf48e5d59a4df13f5
GaugeZich <guilleop.vale@gmail.com>
2023-11-22 09:27:14 -0300

    Agregue la funcionalidad de cerrar sesión

--- src/app/modules/auth/cuenta/cuenta.component.ts
+++ src/app/modules/auth/cuenta/cuenta.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cuenta',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class CuentaComponent {
 
+  constructor(
+    private servicioAuth: AuthService,
+    private router: Router,
+  ){}
+
+  async salir(){
+    const res = await this.servicioAuth.cerrarSesion()
+    .then(res => {
+      alert("Se ha cerrado la sesion correctamente");
+      console.log(res);
+      this.router.navigate(['/inicio'])
+    })
+  }
 }
