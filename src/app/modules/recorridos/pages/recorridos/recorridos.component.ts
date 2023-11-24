78bf7578daff8f5339862ac52fcac9f94dc67dc7
GaugeZich <guilleop.vale@gmail.com>
2023-11-24 11:17:02 -0300

    Agregue los ruteos en la parte de recorrido

--- src/app/modules/recorridos/pages/recorridos/recorridos.component.ts
+++ src/app/modules/recorridos/pages/recorridos/recorridos.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { CrudService } from 'src/app/modules/admin/services/crud.service';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
 
 @Component({
   selector: 'app-recorridos',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class RecorridosComponent {
   opened = false;
+
+  constructor(
+    private authService: AuthService,
+    private crud: CrudService,
+    private router: Router,
+    private auth: AngularFireAuth
+  ){}
+  entrarCuenta(){
+    this.auth.authState.subscribe(usuarios => {
+      if(usuarios) {
+        this.router.navigate(['/cuenta'])
+      }else{
+        this.router.navigate(['/login'])
+      }
+    })
+  }
+
 }
