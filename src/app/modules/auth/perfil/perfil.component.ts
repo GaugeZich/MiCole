1fbef53452ed067a695310d00fc02a59ed4ef932
GaugeZich <guilleop.vale@gmail.com>
2023-11-24 12:02:37 -0300

    Empece con la vista de perfil

--- src/app/modules/auth/perfil/perfil.component.ts
+++ src/app/modules/auth/perfil/perfil.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
+import { CrudService } from '../../admin/services/crud.service';
+import { FirestoreService } from 'src/app/shared/services/firestore.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit{
 
+  coleccionUsuarios: Usuario[] = [];
+
+  usuarioSeleccionado!: Usuario;
+
+  constructor(
+    public servicioCrud: CrudService,
+    public servicioFirestore: FirestoreService,
+    public servicioAuth: AuthService
+  ){}
+
+  async ngOnInit(): Promise<void>{
+    this.servicioAuth.auth.currentUser.then((userLogeado) => {
+      this.servicioCrud.obtenerUsuario().subscribe(usuarios => {
+        if (userLogeado) {
+          const info_cuenta = usuarios.find(usuario => usuario.uid === userLogeado.uid);
+          console.log(info_cuenta);
+        }
+      })
+    })    
+  }
 }
