c9354b1decf04d0201ea92602a661fd3f2fdf427
GaugeZich <guilleop.vale@gmail.com>
2023-11-24 11:04:37 -0300

    Arregle las rutas dentro de configuración

--- src/app/modules/configuracion/pages/configuracion/configuracion.component.ts
+++ src/app/modules/configuracion/pages/configuracion/configuracion.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { CrudService } from 'src/app/modules/admin/services/crud.service';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
 
 
 @Component({
@@ -8,4 +12,20 @@ import { Component } from '@angular/core';
 })
 export class ConfiguracionComponent {
   opened = false;
+
+  constructor(
+    private authService: AuthService,
+    private crud: CrudService,
+    private router: Router,
+    private auth: AngularFireAuth
+  ){}
+  entrarCuenta(){
+    this.auth.authState.subscribe(usuarios => {
+      if(usuarios) {
+        this.router.navigate(['/cuenta'])
+      }else{
+        this.router.navigate(['/login'])
+      }
+    })
+  }
 }
