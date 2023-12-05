f6ee44369b5e9b55ae80f4ca6049e1d3b22061ff
GaugeZich <guilleop.vale@gmail.com>
2023-12-05 17:16:52 -0300

    Comente el TS de información

--- src/app/modules/informacion/pages/informacion/informacion.component.ts
+++ src/app/modules/informacion/pages/informacion/informacion.component.ts
@@ -10,23 +10,31 @@ import { CrudService } from 'src/app/modules/admin/services/crud.service';
   styleUrls: ['./informacion.component.css']
 })
 export class InformacionComponent {
+  // Variable para el sidebar
   opened = false;
 
+  // Arreglo que guardara la tarifa
   tarifaUnica: Tarifa[] = [];
 
   constructor(
+    // Importaciones de los servicios
     public servicioCrud: CrudService,
     private router: Router,
     private auth: AngularFireAuth
   ){}
 
-  // Obtenemos los datos de tarifa
+  // ngOnInit: Se inicializa al abrir el componente 
   ngOnInit(): void{
+    // Obtenemos la tarifa y la guardamos dentro de tarifaUnica para mostrarla en el HTML por interpolación
     this.servicioCrud.obtenerTarifa().subscribe(tarifa => {
       this.tarifaUnica = tarifa;
     })
   }
 
+  /*
+  Esta funcion determina si el usuario esta conectado, si es asi al darle click al boton,
+  te llevara a la vista de "cuenta", de lo contrario te llevara al login
+  */
   entrarCuenta(){
     this.auth.authState.subscribe(usuarios => {
       if(usuarios) {
