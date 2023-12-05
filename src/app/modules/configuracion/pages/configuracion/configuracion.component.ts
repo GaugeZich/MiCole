003ecb90bf39205865ec5745b82ab698a07626e6
LeonelSandoval150 <leonelmaximilianosandoval@gmail.com>
2023-12-05 17:29:48 -0300

    comente el codigo de configuracion

--- src/app/modules/configuracion/pages/configuracion/configuracion.component.ts
+++ src/app/modules/configuracion/pages/configuracion/configuracion.component.ts
@@ -9,12 +9,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./configuracion.component.css']
 })
 export class ConfiguracionComponent {
+  // Variable para el sidebar
   opened = false;
 
   constructor(
+    // Importamos los servicios
     private router: Router,
     private auth: AngularFireAuth
   ){}
+  
+  /*
+  Esta funcion determina si el usuario esta conectado, si es asi al darle click al boton,
+  te llevara a la vista de "cuenta", de lo contrario te llevara al login
+  */
   entrarCuenta(){
     this.auth.authState.subscribe(usuarios => {
       if(usuarios) {
