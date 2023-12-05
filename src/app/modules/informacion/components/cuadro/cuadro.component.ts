27ca92e8caf30f3f06bfb28f456e3c91f6584a57
GaugeZich <guilleop.vale@gmail.com>
2023-12-05 17:20:39 -0300

    Comente el TS del cuadro de información

--- src/app/modules/informacion/components/cuadro/cuadro.component.ts
+++ src/app/modules/informacion/components/cuadro/cuadro.component.ts
@@ -8,14 +8,16 @@ import { CrudService } from 'src/app/modules/admin/services/crud.service';
   styleUrls: ['./cuadro.component.css']
 })
 export class CuadroComponent {
-
+  // Arreglo que guardara las informaciones
   coleccionInformacion: Informacion[] = [];
 
   constructor(
+    // Importación del servicio CRUD
     public servicioCrud : CrudService
   ){}
-
+  // ngOnInit: Se inicializa al abrir el componente
   ngOnInit(): void{
+    // Obtenemos las informaciones y la guardamos en la colección para luego mostrarlas en el cuadro
     this.servicioCrud.obtenerInformacion().subscribe(informacion => {
       this.coleccionInformacion = informacion
     })
