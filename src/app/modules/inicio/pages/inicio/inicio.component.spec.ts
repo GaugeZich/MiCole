83cfaada90d334e76e5c31cc820ff0829908e412
luci0310 <lucianamartinez898@gmail.com>
2023-12-06 01:55:57 -0300

    subo comentarios del html del inicio

--- src/app/modules/inicio/pages/inicio/inicio.component.spec.ts
+++ src/app/modules/inicio/pages/inicio/inicio.component.spec.ts
@@ -6,16 +6,27 @@ describe('InicioComponent', () => {
   let component: InicioComponent;
   let fixture: ComponentFixture<InicioComponent>;
 
+  //se crea una instancia y se asefura que la detección de cambios se ha realizado antes de ejecutar las pruebas
   beforeEach(() => {
+    /*
+    testBed=>crea instancias de componentes y servicios
+    configureTestingModule=>configurar un modulo
+    */
     TestBed.configureTestingModule({
       declarations: [InicioComponent]
     });
+    //crea una instancia del componente
     fixture = TestBed.createComponent(InicioComponent);
+    //proporciona acceso directo a la instancia del componente creado con testbed
     component = fixture.componentInstance;
+    // se utiliza para forzar la detección de cambios en el componente.
     fixture.detectChanges();
   });
-
+  
+  //función Jasmine 
+  //verifica si la instancia del componente se crea correctamente 
   it('should create', () => {
+    //tobeTruthy=>la prueba está verificando que la instancia del componente no sea nula ni indefinida
     expect(component).toBeTruthy();
   });
 });
