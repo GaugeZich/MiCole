249959c3ac8ca4d48b86629ad7e34c9064b606f4
GaugeZich <guilleop.vale@gmail.com>
2023-12-06 04:54:32 -0300

    Comente el TS de list

--- src/app/modules/admin/components/list/list.component.ts
+++ src/app/modules/admin/components/list/list.component.ts
@@ -8,15 +8,20 @@ import { Informacion } from 'src/app/models/informacion';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+  // Creamos una coleccion que esta basada en la interfaz de información
   coleccionInformacion: Informacion[] = [];
 
+  // Variable para guardar la información seleccionada
   informacionSeleccionada!: Informacion;
 
   constructor(
+    // Importamos el servicio CRUD
     public servicioCrud : CrudService
   ){}
 
+  // ngOnInit: Se inicializa al abrir el componente
   ngOnInit(): void{
+    // Obtiene la información y la guarda en la colección
     this.servicioCrud.obtenerInformacion().subscribe(informacion => {
       this.coleccionInformacion = informacion
     })
@@ -26,18 +31,15 @@ export class ListComponent {
   // Guarda la informacion seleccionada para borrar la información
   confirm(informacionSeleccionada: Informacion){
     this.informacionSeleccionada = informacionSeleccionada
-
+    // Si la respuesta es afirmativa (verdad/true) entonces borrara la información llamando a la función
     if(confirm ("¿Está seguro?") === true){
       this.borrarInformacion()
     }
-
-    // let respuesta = confirm("¿Está seguro?")
-    // if(respuesta){
-    //   this.borrarInformacion()
-    // }
   }
 
+  // Función para borrar la información 
   borrarInformacion(){
+    // Borra la información con el servicio CRUD
     this.servicioCrud.eliminarInformacion(this.informacionSeleccionada.idInformacion)
     .then(res => {
       alert("La información ha sido elminada correctamente!");
