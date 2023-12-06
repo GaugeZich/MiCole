a8856a7dd77d0d785e0a19af490e8edf27b60b35
luci0310 <lucianamartinez898@gmail.com>
2023-12-06 01:35:52 -0300

    subo comentarios del ts del login

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -34,19 +34,23 @@ export class LoginComponent {
  
   //creo la función para inciciar sesión 
   async iniciar(){
+    // Guardamos las credenciales (email y contraseña)
     const credenciales={
       email: this.usuarios.email,
       contrasena:this.usuarios.contrasena
     };
 
+    // Llamamos la función registrar del servicio Auth
     const res =await this.servicioAuth.iniciarSesion(credenciales.email, credenciales.contrasena)
     .then(res=>{
+      //alerta para que el usuario sepa que pudo acceder
       alert("Ha accedido con éxito :) ");
       console.log(credenciales.email);
       //redirigimos a la vista inicial 
       this.Router.navigate(['/inicio']);
     })
     .catch(error=>{
+      //alerta para que el usuario sepa que hubo un problema
       alert("Hubo un error a inicial sesión :( \n"+error);
       console.log(credenciales.email);
     })
