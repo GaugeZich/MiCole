d588999cea5ec00537d96609a4628a897315af39
GaugeZich <guilleop.vale@gmail.com>
2023-12-07 01:59:45 -0300

    Comente el codigo de firestore service

--- src/app/shared/services/firestore.service.ts
+++ src/app/shared/services/firestore.service.ts
@@ -7,23 +7,27 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
     providedIn: 'root'
 })
 export class FirestoreService{
-    // referenciamos colección en la base de datos
+    // Referenciamos colección en la base de datos
     private usuariosCollection: AngularFirestoreCollection<Usuario>
 
+    // Constructor que inyecta AngularFirestore y AngularFireAuth
     constructor(private database: AngularFirestore, private afAuth: AngularFireAuth){
-        this.usuariosCollection=this.database.collection<Usuario>('usuarios')
+      // Inicia la colección de usuarios
+      this.usuariosCollection=this.database.collection<Usuario>('usuarios')
     }
 
-    
+    // Función para agregar un usuario a firestore
     agregarUsuario(usuario: Usuario, id: string){
     return new Promise(async(resolve, reject) => {
-      //capturamos los datos 
+      // Captura de los datos
       try{
+        // Asigna el UID 
         usuario.uid = id;
+        // Agrega el usuario a la colección
         const resultado = await this.usuariosCollection.doc(id).set(usuario);
-        resolve(resultado)
+        resolve(resultado) // Resuelve la promesa
       } catch (error){
-        reject(error)
+        reject(error) // Rechaza la promesa
       }
     })
   }
@@ -31,12 +35,14 @@ export class FirestoreService{
   // Funcion para eliminar el autenticador de firestore
   async eliminarAutenticador(uid: string){
     try{
+      // Obtiene el usuario conectado
       const user = await this.afAuth.currentUser;
       if(user){
+        // Si hay un usuario conectado lo elimina
         await user.delete();
       }
     }catch(error){
-      throw error;
+      throw error; // Lanza un error
     }
   }
 }
